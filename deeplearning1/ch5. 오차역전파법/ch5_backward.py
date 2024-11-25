b4fe6bea84f10b35eb8d8988ea9a6b2a4f1a18ec
seungdeng <sgrhee3@naver.com>
2024-11-26 02:59:01 +0900

    ch5 신경망 예제 (backward)

--- deeplearning1/ch5. 오차역전파법/ch5_backward.py
+++ deeplearning1/ch5. 오차역전파법/ch5_backward.py
@@ -49,38 +49,68 @@ class AddLayer:
         dx = dout * 1
         dy = dout * 1
         return dx,dy
+# =============================================================================
+#     
+# apple = 100
+# apple_num = 2
+# orange = 150
+# orange_num = 3
+# tax = 1.1
+# 
+# mul_apple_layer = MulLayer()            
+# mul_orange_layer = MulLayer()           
+# add_apple_orange_layer = AddLayer()     
+# mul_tax_layer = MulLayer()
+# 
+# apple_price = mul_apple_layer.forward(apple, apple_num)                 #1
+# orange_price = mul_orange_layer.forward(orange, orange_num)             #2
+# all_price = add_apple_orange_layer.forward(apple_price, orange_price)   #3
+# price = mul_tax_layer.forward(all_price, tax)                           #4
+# 
+# dprice = 1
+# dall_price, dtax = mul_tax_layer.backward(dprice)                           #4
+# dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price)   #3
+# dorange, dorange_num = mul_orange_layer.backward(dorange_price)             #2
+# dapple, dapple_num = mul_apple_layer.backward(dapple_price)                 #1
+# 
+# print(price)    #715
+# print(dapple_num,dapple,dorange,dorange_num,dtax)   # 110 2.2 3.3 165 650
+# =============================================================================
+
+import numpy as np
+
+class Relu:
+    def __init__(self):
+        self.mask = None
     
-apple = 100
-apple_num = 2
-orange = 150
-orange_num = 3
-tax = 1.1
-
-mul_apple_layer = MulLayer()            
-mul_orange_layer = MulLayer()           
-add_apple_orange_layer = AddLayer()     
-mul_tax_layer = MulLayer()
-
-apple_price = mul_apple_layer.forward(apple, apple_num)                 #1
-orange_price = mul_orange_layer.forward(orange, orange_num)             #2
-all_price = add_apple_orange_layer.forward(apple_price, orange_price)   #3
-price = mul_tax_layer.forward(all_price, tax)                           #4
-
-dprice = 1
-dall_price, dtax = mul_tax_layer.backward(dprice)                           #4
-dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price)   #3
-dorange, dorange_num = mul_orange_layer.backward(dorange_price)             #2
-dapple, dapple_num = mul_apple_layer.backward(dapple_price)                 #1
-
-print(price)    #715
-print(dapple_num,dapple,dorange,dorange_num,dtax)   # 110 2.2 3.3 165 650
-
-
-
+    def forward(self,x):
+        self.mask = (x <=0)
+        out = x.copy()
+        out[self.mask] = 0
+        
+        return out
+    
+    def backward(self,dout):
+        dout[self.mask] = 0
+        dx = dout
+        
+        return dx
     
     
+class Sigmoid:
+    def __init__(self):
+        self.out = None
     
+    def forward(self,x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        
+        return out
     
+    def backward(self,dout):
+        dx = dout * (1.0 - self.out ) * self.out
+        
+        return dx
     
     
     
