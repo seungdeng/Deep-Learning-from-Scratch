894bff9fe95b37f971626bb61ccad2f7780644dc
seungdeng <sgrhee3@naver.com>
2024-11-28 23:09:32 +0900

    ch6.

--- deeplearning1/ch6. 학습 관련 기술들/ch6_skills.py
+++ deeplearning1/ch6. 학습 관련 기술들/ch6_skills.py
@@ -27,19 +27,37 @@ class SGD:
 #     params = network.params
 #     optimizer.update(params,grads)
 # =============================================================================
+# =============================================================================
+# 
+# class Momentum:
+#     def __init__(self,lr=0.01,momentum = 0.9):
+#         self.lr = lr
+#         self.momentum = momentum
+#         self.v = None
+#         
+#     def update(self,params,grads):
+#         if self.v is None:
+#             self.v = {}
+#             for key,val in parmas.items():
+#                 self.v[key] = np.zeros_like(val)
+#             
+#         for key in params.keys():
+#             self.v[key] = self.momentum * self.v[key] - self.lr * grads[key]
+#             params[key] += self.v[key]
+# =============================================================================
 
-class Momentum:
-    def __init__(self,lr=0.01,momentum = 0.9):
+class AdaGrad:
+    def __init__(self, lr= 0.01):
         self.lr = lr
-        self.momentum = momentum
-        self.v = None
+        self.h = None
         
     def update(self,params,grads):
-        if self.v is None:
-            self.v = {}
-            for key,val in parmas.items():
-                self.v[key] = np.zeros_like(val)
-            
+        if self.h is None:
+            self.h = {}
+            for key,val in params.items():
+                self.h[key] = np.zeros_like(val)
+        
         for key in params.keys():
-            self.v[key] = self.momentum * self.v[key] - self.lr * grads[key]
-            params[key] += self.v[key]
+            self.h[key] += grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key] + 1e-7))
+            
