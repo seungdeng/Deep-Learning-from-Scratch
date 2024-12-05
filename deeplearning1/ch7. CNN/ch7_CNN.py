1f6625ee709cbffaf9c95151382a8e79d8c7b8a3
seungdeng <sgrhee3@naver.com>
2024-12-06 00:10:34 +0900

    ch7. CNN 학습
    챕터 마무리

--- deeplearning1/ch7. CNN/ch7_CNN.py
+++ deeplearning1/ch7. CNN/ch7_CNN.py
@@ -72,3 +72,58 @@ class Pooling:
         
         return out
         
+class SimpleConvNet:
+    def __init__(self, input_dim = (1,28,28),
+                 conv_param={'filter_num':30, 'filter_size':5,
+                             'pad':0, 'stride':1},
+                 hidden_size =100, output_size=10, weight_int_std=0.01):
+        filter_num = conv_param['filter_num']
+        filter_size = conv_param['filter_size']
+        filter_pad = conv_param['pad']
+        filter_stride = conv_param['stride']
+        input_size = input_dim[1]
+        
+        conv_output_size(input_size - filter_size + 2*filter_pad) /\
+            filter_stride + 1
+        pool_output_size = int(filter_num * (conv_output_size/2) *
+                               (conv_output_size/2))
+        
+        
+        
+        
+self.param = {}
+self.params['W1'] = weight_int_std *\
+    np.random.rand(filter_num, input_din[0], filter_size, filter_size)
+self.params['b1'] = np.zeros(filter_num)
+self.params['W2'] = weight_int_std *\
+    np.random.rand(pool_output_size, hidden_size)
+self.params['b2'] = np.zeros(hidden_size)
+self.params['W3'] = weight_int_std *\
+    np.random.rand(hidden_size, output_size)
+self.params['b3'] = np.zeros(output_size)
+
+
+
+
+
+
+self.layers = OrderedDict()
+self.layers['Conv1'] = Convolution(self.params['W1'], self.params['b1'],
+                                   conv_param['stride'],conv_param['pad'])
+self.layers['Relu1'] = Relu()
+self.layers['Pool1'] = Pooling(pool_h=2,pool_w=2,stride=2)
+self.layers['Affine1'] = Affine(self.params['W2'],self.params['b2'])
+self.layers['Relu2'] = Relu()
+self.layers['Affine2'] = Affine(self.params['W3'],
+                                self.params['b3'])
+self.last_layer = SoftmaxWithLoss()
+
+
+def predict(self,x):
+    for layer in self.layers.values():
+        x = layer.forward(x)
+        return x
+    
+    def lostt(self,x,t):
+        y = self.predict(x)
+        return self.last_layer.forward(y,t)
