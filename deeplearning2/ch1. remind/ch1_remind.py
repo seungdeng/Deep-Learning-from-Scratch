6518542998baec8d2a6f56b422c0db53aa6b2b7a
seungdeng <sgrhee3@naver.com>
2024-12-14 22:35:52 +0900

    Update ch1_remind.py

--- deeplearning2/ch1. remind/ch1_remind.py
+++ deeplearning2/ch1. remind/ch1_remind.py
@@ -103,3 +103,18 @@ class MatMul:
         dW = np.matmul(self.x.T,dout)
         self.grads[0][...] = dW
         return dx
+    
+    
+class Sigmoid:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+        
+    def forward(self,x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+    
+    def backward(self,dout):
+        dx = dout*(1.0 - self.out) * self.out
+        return dx
