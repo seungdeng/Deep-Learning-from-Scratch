21ee7a1f962be5b81bfbe8cf42d9660c0dc5aa97
seungdeng <sgrhee3@naver.com>
2024-12-15 22:08:18 +0900

    ch2. 자연어 처리(NLP)
    실습 코드

---
+++ deeplearning2/ch2. about NLP/ch2_NLP.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+
+text = 'You say goodbye and I say Hello.'
+
+text = text.lower()
+text = text.replace('.', ' .')
+# =============================================================================
+# print(text)
+# =============================================================================
+words = text.split(' ')
+# =============================================================================
+# print(words)
+# =============================================================================
+
+word_to_id = {}
+id_to_word = {}
+
+for word in words:
+    if word not in word_to_id:
+        new_id = len(word_to_id)
+        word_to_id[word] = new_id
+        id_to_word[new_id] = word
+
+
+corpus = [word_to_id[w] for w in words]
+corpus = np.array(corpus)
+
+def preprocess(text):
+    text = text.lower()
+    text = text.replace('.', ' .')
+    words = text.split(' ')
+
+    word_to_id = {}
+    id_to_word = {}
+
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
+
+    corpus = np.array([word_to_id[w] for w in words])
+   
+    return corpus,word_to_id,id_to_word
