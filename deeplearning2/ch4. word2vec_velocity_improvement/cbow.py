a93482a50655c17b63645d9c12f2e8540da7af0f
SeungKeon Lee <113653735+seungdeng@users.noreply.github.com>
2024-12-24 15:54:10 +0900

    Update chap4. word2vec 속도 개선

---
+++ deeplearning2/ch4. word2vec_velocity_improvement/cbow.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 24 15:11:48 2024
+
+Created by SeungKeon Lee
+"""
+
+import sys
+from negative_sampling_layer import NegativeSamplingLoss
+sys.path.append('..')
+import numpy as np
+from common.layers import Embedding
+
+class CBOW:
+    def __init__(self,vocab_size,hidden_size,window_size,corpus):
+        V, H = vocab_size, hidden_size
+        
+        #가중치 초기화
+        W_in = 0.01 * np.random.randn(V,H).astype('f')
+        W_out = 0.01 * np.random.randn(V,H).astype('f')
+        
+        #layer 생성
+        self.in_layers = []
+        for i in range(2 * window_size):
+            layer = Embedding(W_in)     #Embedding 계층 사용
+            self.in_layers.append(layer)
+        self.ns_loss = NegativeSamplingLoss(W_out, corpus,power = 0.75,sample_size = 5)
+        
+        #모든 가중치와 기울기를 배열에 모은다
+        layers = self.in_layers + [self.ns_loss]
+        self.params, self.grads = [], []
+        for layer in layers:
+            self.params += layer.params
+            self.grads += layer.grads
+            
+        #인스턴스 변수에 단어의 분산 표현을 저장한다.
+        self.word_vecs = W_in
+        
+    def forward(self,contexts,target):
+        h = 0
+        for i,layer in enumerate(self.in_layers):
+            h += layer.forward(contexts[:,i])
+            loss = self.ns_loss.forward(h, target)
+        return loss
+        
+    def backward(self,dout = 1):
+        dout = self.ns_loss.backward()
+        dout *= 1 / len(self.in_layers)
+        for layer in self.in_layers:
+            layer.backward(dout)
+        return None    
+        
+            
--- deeplearning2/ch4. word2vec_velocity_improvement/ch4_word2vec_velocity_improvement.py
+++ deeplearning2/ch4. word2vec_velocity_improvement/ch4_word2vec_velocity_improvement.py
@@ -13,3 +13,69 @@ print(W[5])
 
 idx = np.array([1,0,3,0])
 print(W[idx])
+
+class Embedding:
+    def __init__(self,W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+        
+        def forward(self,idx):
+            W, = self.params
+            self.idx = idx
+            out = W[idx]
+            return out
+# =============================================================================
+#         
+#         def backward(self,dout):
+#             dW, =  self.grads
+#             dW[...] = 0
+#             dW[self.idx] = dout
+#             return None
+# =============================================================================
+
+
+        def backward(self,dout):
+            dW, =  self.grads
+            dW[...] = 0
+      
+            for i, word_id in enumerate(self.idx):
+                dW[word_id] += dout[i]
+                
+            #혹은
+            # np.add.at(dW,self.idx,dout)
+            return None
+        
+        
+#0에서 9까지의 숫자 중 하나를 무작위로 샘플링        
+np.random.choice(10)
+np.random.choice(10)
+
+#words에서 하나만 무작위로 샘플링
+words = ['you','say','goodbye','I','hello','.']
+np.random.choice(words)
+
+#5개만 무작위로 샘플링(중복 있음)
+np.random.choice(words,size = 5)
+
+# '' (중복 없음)
+np.random.choice(words, size = 5, replace = False)
+
+#확률분포에 따라 샘플링
+p = [0.5,0.1,0.05,0.2,0.05,0.1]
+np.random.choice(words, p = p)
+
+p =[0.7,0.29,0.01]
+new_p = np.power(p,0.75)
+new_p /= np.sum(new_p)
+print(new_p)
+
+
+from negative_sampling_layer import UnigramSampler
+corpus = np.array([0,1,2,3,4,1,2,3])
+power = 0.75
+sample_size = 2
+sampler = UnigramSampler(corpus,power,sample_size)
+target = np.array([1,3,0])
+negative_sample = sampler.get_negative_sample(target)
+print(negative_sample)
