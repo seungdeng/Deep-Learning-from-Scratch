2a989217379c4bea9ad9474e5aa28bbb56dd9625
SeungKeon Lee <113653735+seungdeng@users.noreply.github.com>
2024-12-24 13:55:10 +0900

    Update ch4_word2vec_velocity_improvement.py
    word2vec개선점1-Embedding 도입 ->MatMul계층 개선

--- deeplearning2/ch4. word2vec_velocity_improvement/ch4_word2vec_velocity_improvement.py
+++ deeplearning2/ch4. word2vec_velocity_improvement/ch4_word2vec_velocity_improvement.py
@@ -5,3 +5,11 @@ Created on Mon Dec 23 18:09:10 2024
 Created by SeungKeon Lee
 """
 
+import numpy as np
+W = np.arange(21).reshape(7, 3)
+print(W)
+print(W[2])
+print(W[5])
+
+idx = np.array([1,0,3,0])
+print(W[idx])
