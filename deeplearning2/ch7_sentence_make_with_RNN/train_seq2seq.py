2c35e70637626498d56cb206f0eaed4c3544a7b2
seungdeng <sgrhee3@naver.com>
2025-01-03 23:01:11 +0900

    Update train_seq2seq.py
    peeky, reverse 적용한 학습 훈련 적용

--- deeplearning2/ch7_sentence_make_with_RNN/train_seq2seq.py
+++ deeplearning2/ch7_sentence_make_with_RNN/train_seq2seq.py
@@ -33,8 +33,8 @@ max_epoch = 25
 max_grad = 5.0
 
 # 일반 혹은 엿보기(Peeky) 설정 =====================================
-model = Seq2seq(vocab_size, wordvec_size, hidden_size)
-# model = PeekySeq2seq(vocab_size, wordvec_size, hidden_size)
+#model = Seq2seq(vocab_size, wordvec_size, hidden_size)
+model = PeekySeq2seq(vocab_size, wordvec_size, hidden_size)
 # ================================================================
 optimizer = Adam()
 trainer = Trainer(model, optimizer)
